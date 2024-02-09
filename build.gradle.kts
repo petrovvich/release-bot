19e6695aa854b1ca3ed7462e616e5fdaf464afe9
petrovvich <alexromantrue@gmail.com>
2024-02-09 08:52:46 -0300

    gradle version updated to 8.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ version = "0.0.1-SNAPSHOT"
 description = "release-bot"
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "3.7.0"
     id("io.micronaut.test-resources") version "3.7.0"
 }
